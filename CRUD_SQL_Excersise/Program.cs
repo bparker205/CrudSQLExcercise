7e0e49ff092c2a975c55809cf4482f9157aef9cc
Brian <brian@theparkerbunch.com>
2018-09-21 19:07:53 -0500

    Add project files.

---
+++ CRUD_SQL_Excersise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUD_SQL_Excersise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
